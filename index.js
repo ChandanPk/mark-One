210cb658a05af22f21e0e91036de37ed1e76d3d6
Chandan kumar <chandanpk9@gmail.com>
2021-08-19 09:34:12 +0000

    Add prompt for usernName

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const readlineSync = require('readline-sync');
 
 var score = 0;
+var userName = readlineSync.question("Please enter your name: \n");
+
 
 function ask(question){
   return readlineSync.question(question);
@@ -11,12 +13,19 @@ function ask(question){
 const questions = [
 
   {question: "1. Is my name chandan (yes / no)? ", answer:"no"},
+
   {question: "2. Do i leave in the city Bnglore(yes / no)?", answer:"yes"},
+
   {question: "3. Do i got any sorta pets (yes / no)? ", answer:"no"},
+
   {question: "4. what about my girlFriends, do i have any (yes / no)? ", answer:"no"},
+
   {question: "5. I'm i vegeterian (yes / no)? ", answer:"no"},
+
   {question: "6. Topper in schools & colleges(yes / no)? ", answer:"no"},
+
   {question: "7. Do i extremly hate PUBG (yes / no)? ", answer:"yes"},
+
   {question: "8. You think i Love Cooking (yes / no)? ", answer:"yes"}
 
 ]
@@ -31,4 +40,4 @@ for(var i=0; i < questions.length; i++){
   }
 }
 
-console.log(`Your Score is: ${score} out of 8`)
+console.log(`${userName} your Score is: ${score} out of 8`)
