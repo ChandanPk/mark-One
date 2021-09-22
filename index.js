2237f7a05930ca376f5b6b4da65849aadefcbfac
Chandan kumar <chandanpk9@gmail.com>
2021-09-22 17:37:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function ask(question){
 
 const questions = [
 
-  {question: "1. Is my name chandan (yes / no)? ", answer:"no"},
+  {question: "1. Is my surname 'yadav' (yes / no)? : ", answer:"no"},
 
-  {question: "2. Do i leave in the city Bnglore(yes / no)?", answer:"yes"},
+  {question: "2. Do i leave in the city Banglore(yes / no)?", answer:"yes"},
 
-  {question: "3. Do i got any sorta pets (yes / no)? ", answer:"no"},
+  {question: "3. Do i got any pets (yes / no)? ", answer:"no"},
 
-  {question: "4. what about my girlFriends, do i have any (yes / no)? ", answer:"no"},
+  {question: "4. what about girlFriends, do i have any (yes / no)? ", answer:"no"},
 
   {question: "5. I'm i vegeterian (yes / no)? ", answer:"no"},
 
@@ -30,14 +30,18 @@ const questions = [
 
 ]
 
-console.log("--> How well do you know me ?, let's find out. <--\n")
+console.log(`--> How well do you know me ${userName}?, let's find out. <--\n`)
 
 for(var i=0; i < questions.length; i++){
   let userAnswer = ask(questions[i].question);
 
   if (userAnswer.toLowerCase() === questions[i].answer){
     score++
+    console.log(`Yay!\nYour current score is: ${score}`);
+  }
+  else {
+    console.log(`oops!\nYour current score is: ${score}`);
   }
 }
 
-console.log(`${userName} your Score is: ${score} out of 8`)
+console.log(`\n\n Your Final Score is: ${score} out of 8`)
